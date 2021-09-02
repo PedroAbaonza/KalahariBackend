71dbcc43fcb596b50633ae62d50444d759cfd4b5
atjesugq <jesus.gonzalez@agilethought.com>
2021-09-02 10:57:26 -0500

    Se cambio el path para la consulta de grupos por examen por tecnologia
    Se unificaron las ramas de vinculacion_group_dashboard y la de hector_cambios
    vista_examen para preparar el merge final de backend. Al aceptar este commit
    en un pull request funcionaran los componentes de single group dashboard y de
    single person dashboard, que necesitan los datos que se procesan en este commit.

--- src/main/java/com/agilethought/kalahari/controllers/ExamenTecnologiaController.java
+++ src/main/java/com/agilethought/kalahari/controllers/ExamenTecnologiaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
-@RequestMapping("/examen")
+@RequestMapping("/examen-tecnologia")
 public class ExamenTecnologiaController {
 
     @Autowired
