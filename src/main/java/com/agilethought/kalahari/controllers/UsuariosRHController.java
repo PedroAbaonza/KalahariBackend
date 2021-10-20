9b1a06a80eb04ac987b74bdf4371f1fd75f1ea74
Pedro Emmanuel Abaonza Juarez <emmanuelabaonza@gmail.com>
2021-10-20 16:23:24 -0500

    Cambios en app engine

--- src/main/java/com/agilethought/kalahari/controllers/UsuariosRHController.java
+++ src/main/java/com/agilethought/kalahari/controllers/UsuariosRHController.java
@@ -15,38 +15,46 @@ import java.util.Optional;
 public class UsuariosRHController {
     @Autowired
     UsuarioRHService usuarioRHService;
+
     @GetMapping
     public ArrayList<T013UsuariosRH> obtenerUsuariosRH() {
         return usuarioRHService.obtenerUsuariosRH();
     }
+
     @GetMapping("/{id}")
     public Optional<T013UsuariosRH> obtenerUsuariosRHid(@PathVariable String id) {
         return usuarioRHService.obtenerPorId(id);
     }
+
     @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping
-    public String agregarUsuario(@RequestBody T013UsuariosRH t013UsuariosRH){
+    public String agregarUsuario(@RequestBody T013UsuariosRH t013UsuariosRH) {
         return usuarioRHService.agregarUsuarioRH(t013UsuariosRH);
     }
+
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/getusuario/{email}")
-    public T013UsuariosRH obtenerUsuariorhemail(@PathVariable String email){
-        return usuarioRHService.obtenerPoremail(email);
+    public T013UsuariosRH obtenerUsuariorhemail(@PathVariable String email) {
+        T013UsuariosRH user = usuarioRHService.obtenerPoremail(email);
+        return user;
     }
+
     @CrossOrigin(origins = "http://localhost:4200")
     @PutMapping("/intento/{email}")
-    public String aumentocont(@PathVariable String email){
+    public String aumentocont(@PathVariable String email) {
         return usuarioRHService.aumentoCount(email);
     }
+
     @CrossOrigin(origins = "http://localhost:4200")
     @PutMapping("/bloqueo/{email}")
-    public  String disableUsuario(@PathVariable String email){
-        return  usuarioRHService.disableUsuariorh(email);
+    public String disableUsuario(@PathVariable String email) {
+        return usuarioRHService.disableUsuariorh(email);
     }
+
     @CrossOrigin(origins = "http://localhost:4200")
     @PutMapping("/default/{email}")
-    public  String enableUsuario(@PathVariable String email){
-        return  usuarioRHService.enableUsuariorh(email);
+    public String enableUsuario(@PathVariable String email) {
+        return usuarioRHService.enableUsuariorh(email);
     }
 
 
